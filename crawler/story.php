0d4623efba5f0e34f66e2181e01d82e497ff1b10
amose <ligl@gammainfo.com>
2015-06-08 08:47:30 +0800

    添加小说获取

--- crawler/story.php
+++ crawler/story.php
@@ -4,3 +4,80 @@
  * Date: 15/6/6
  * Time: 上午11:43
  */
+set_time_limit(0);//永不过期
+
+include_once('../libs/simple_html_dom.php');
+include_once('../libs/ez_sql_core.php');
+include_once('../libs/ez_sql_mysql.php');
+include_once('../libs/utils.php');
+include_once('../config.php');
+include_once('../libs/GLogger.class.php');
+header("Content-type: text/html; charset=utf-8");
+//error_reporting(0);
+$db = new ezSQL_mysql($_CONFIG['db']['db_user'], $_CONFIG['db']['db_password'], $_CONFIG['db']['db_name'], $_CONFIG['db']['db_host'], $_CONFIG['db']['encoding']);
+
+$movie_id = $db->get_var('select max(out_id) as out_id from video');//影片编号
+$movie_id = intval($movie_id) == 0 ? 1111 : $movie_id;
+$fetch_rows = 2000;
+$html = new simple_html_dom();
+$cat_page=array('23'=>25,'24'=>17,'25'=>31,'26'=>19,'27'=>18,'28'=>17,'29'=>17,'30'=>17);
+foreach($cat_page as $cid=>$p){
+    $page=$p;
+    while($page--){
+        //TODO 获取行表
+        $page_index =$p;
+        if($page>1){
+            $cid.='_'.$page;
+        }
+        $url="http://www.gtb8.com/html/part/index$page_index.html";
+        $content = curl_get($url);
+        $content = iconv("GBK", "UTF-8//IGNORE", $content);
+        $html->load($content);
+        if (!is_object($html)) {
+            continue;
+        }
+
+    }
+
+}
+while ($movie_id++ > 0 && --$fetch_rows > 0) {
+
+    $url = "http://www.gtb8.com/html/part/index.html";
+    $content = curl_get($url);
+    $content = iconv("GBK", "UTF-8//IGNORE", $content);
+    $html->load($content);
+    if (is_object($html)) {
+        // 存在
+        $jq = $html->find("div#jq", 0);
+        if (!$jq) {
+            GLogger::e('【小说】网页解析失败', $url);
+            continue;
+        }
+    } else {
+        // not found
+        GLogger::e('【小说】地址不存在', $url);
+    }
+    //隔一秒一循环
+    sleep(1);
+}
+//TODO 如果地址不能访问，发送邮件通过我
+function get_cat_id($category)
+{
+    global $db;
+    $cat = $db->get_row("select * from category where `name`='$category'");
+    if (!$cat) {
+        // 插入
+        $cat_data['name'] = $category;
+        $cat_data['type'] = 2;
+        $cat_data['join_time'] = millisecond();
+        $rlt = $db->query("INSERT INTO category SET " . $db->get_set($cat_data));
+        if ($rlt) {
+            $cat_id = $db->insert_id;
+        } else {
+            $cat_id = 0;
+        }
+    } else {
+        $cat_id = $cat->id;
+    }
+    return $cat_id;
+}
--- crawler/video.php
+++ crawler/video.php
@@ -14,29 +14,43 @@ include_once('../config.php');
 include_once('../libs/GLogger.class.php');
 header("Content-type: text/html; charset=utf-8");
 //error_reporting(0);
-$db = new ezSQL_mysql($_CONFIG['db']['db_user'], $_CONFIG['db']['db_password'], $_CONFIG['db']['db_name'], $_CONFIG['db']['db_host'],$_CONFIG['db']['encoding']);
+$db = new ezSQL_mysql($_CONFIG['db']['db_user'], $_CONFIG['db']['db_password'], $_CONFIG['db']['db_name'], $_CONFIG['db']['db_host'], $_CONFIG['db']['encoding']);
 
 $movie_id = $db->get_var('select max(out_id) as out_id from video');//影片编号
-$movie_id = intval($movie_id) == 0 ? 1000 : $movie_id;
-$fetch_rows = 1000;
-
+$movie_id = intval($movie_id) == 0 ? 1111 : $movie_id;
+$fetch_rows = 10000;
 $html = new simple_html_dom();
-do {
+while ($movie_id++ > 0 && --$fetch_rows > 0) {
     $url = "http://www.gtb8.com/view/index$movie_id.html";
     $content = curl_get($url);
-    $content = iconv("GBK","UTF-8//IGNORE",$content);
+    $content = iconv("GBK", "UTF-8//IGNORE", $content);
     $html->load($content);
     if ($html) {
         // 存在
         $jq = $html->find("div#jq", 0);
+        if (!$jq) {
+            GLogger::e('【视频】网页解析失败', $url);
+            continue;
+        }
         $info = $jq->find('div.infobox .info', 0);
+        if (!$info) {
+            GLogger::e('【视频】网页解析失败', $url);
+            continue;
+        }
         $title = trim($info->children(0)->plaintext);
         $category = trim($info->children(2)->find('a', 0)->plaintext);
         $url = "http://www.gtb8.com/video/?$movie_id-0-0.html";
         $content = curl_get($url);
-        $content = iconv("GBK","UTF-8//IGNORE",$content);
+        $content = iconv("GBK", "UTF-8//IGNORE", $content);
         $html->load($content);
-        $video_list = $html->find('div.playerbox>.play655', 0)->find('script', 0)->innertext;
+        if (!is_object($html)) {
+            continue;
+        }
+        $video_list = $html->find('div.playerbox>.play655', 0);
+        if (!is_object($video_list)) {
+            continue;
+        }
+        $video_list = $video_list->find('script', 0)->innertext;
         $regex = '/\[\[.*\]\]/';
         if (preg_match($regex, $video_list, $matches)) {
             $cat_id = get_cat_id($category);
@@ -59,21 +73,22 @@ do {
                         $v_data['join_time'] = millisecond();
                         $rlt = $db->query("INSERT INTO video SET " . $db->get_set($v_data));
                         if (!$rlt) {
-                            GLogger::e('插入视频失败', $v_data);
+                            GLogger::e('【视频】插入视频失败', $v_data);
                         }
                     }
                 }
             }
         } else {
-            GLogger::e('没有视频源', $url);
+            GLogger::e('【视频】没有视频源', $url);
         }
         // var VideoListJson=[['xfplay',['\u7B2C1\u96C6$xfplay://dna=meEbAGIfmdjbAGbgmxfcDZx5AdyeBefbAHD5mwa2meHWAwEeAxD0At|dx=107518546|mz=7405_onekeybatch.mp4|zx=nhE0pdOVl3P5AF5uLKP5rv5wo206BGa4mc94MzXPozS|zx=nhE0pdOVl3Ewpc5xqzD4AF5wo206BGa4mc94MzXPozS$xfplay']]],urlinfo='http://'+document.domain+'/video/?4959-<from>-<pos>.html';
     } else {
         // not found
-        GLogger::e('视频地址不存在', $url);
+        GLogger::e('【视频】地址不存在', $url);
     }
-    $movie_id++;
-} while (--$fetch_rows > 0);
+    //隔一秒一循环
+    sleep(1);
+}
 //TODO 如果地址不能访问，发送邮件通过我
 //foreach ($html->find('img') as $element)
 //    echo $element->src . '<br>cd';
