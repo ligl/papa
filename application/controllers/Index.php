9a59f15f4b015af5597789c5576dee0366659b9a
amose <ligl@gammainfo.com>
2015-07-19 23:58:24 +0800

    top

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -11,6 +11,8 @@ class Index extends HomeBase
 
     public function index()
     {
+        //http://7xkhs6.com1.z0.glb.clouddn.com/123456.mp4 优衣库 试衣间
+        //http://7xkbqi.com1.z0.glb.clouddn.com/yangmi.mp4 迷奸 酷似杨幂
         //ads
         $cur_time = $this->millisecond();
         $query = $this->db->query('select * from ads where status=0 and expire_time>? order by weight desc',array($cur_time));
@@ -21,6 +23,9 @@ class Index extends HomeBase
         }
         $this->assign('ads', $ads);
 
+        //top
+        $topList = $this->getTop();
+        $this->assign('topList',$topList);
         //story
         $query=$this->db->query('select *,2 as p_type from story where weight>=0 order by weight desc limit 20');
         if ($query->num_rows() > 0) {
@@ -41,6 +46,15 @@ class Index extends HomeBase
         $this->display();
     }
 
+    private function getTop(){
+        $query=$this->db->query('select id,title,cat_id,cat_name,content,guid,weight,2 as p_type from story where is_top=1 and weight>=0 union all select id,title,cat_id,cat_name,url as content,guid,weight,1 as p_type from video where is_top=1 and weight>=0');
+        if ($query->num_rows() > 0) {
+            $list = $query->result_array();
+        } else {
+            //TODO no
+        }
+        return $list;
+    }
     /**
      * story详页
      * @param $guid
