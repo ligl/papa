1bb29c9b13c3d218afe246083a670c262267ac5d
amose <ligl@gammainfo.com>
2015-07-20 00:16:12 +0800

    top

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -30,11 +30,17 @@ class Welcome extends MY_Controller {
 	}
 
     public function test(){
-        $data["title"]="标题";
-        $data["num"]="123123";
-
-        $this->assign('data',$data);
-
-        $this->display("test.html");
+        echo $this->guid();
+    }
+    function guid()
+    {
+        mt_srand((double)microtime() * 10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $uuid = substr($charid, 0, 8)
+            . substr($charid, 8, 4)
+            . substr($charid, 12, 4)
+            . substr($charid, 16, 4)
+            . substr($charid, 20, 12);
+        return $uuid;
     }
 }
